f29666e6c3c892983fd2af07c36d0845ea7d942e
Abhishek Kumar Rai <122802561+abhishekrai69@users.noreply.github.com>
2024-12-25 22:59:45 +0530

    Create Day 2 - Move All Zeroes to End.java

---
+++ GFG - 160 (Arrays)/Day 2 - Move All Zeroes to End.java
@@ -0,0 +1,15 @@
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        int n = arr.length;
+        int nonZeroIndex = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[nonZeroIndex];
+                arr[nonZeroIndex] = arr[i];
+                arr[i] = temp;
+                nonZeroIndex++;
+            }
+        }
+    }
+}
