73b23323bbcb68a7e91842dc58ba17df5add5014
Abhishek Kumar Rai <122802561+abhishekrai69@users.noreply.github.com>
2024-12-28 21:44:05 +0530

    Create Day 11 - Maximum Product Subarray.java

---
+++ GFG - 160 (Arrays)/Day 11 - Maximum Product Subarray.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    int maxProduct(int[] nums) {
+        int maxProduct = nums[0];
+        int maxVal = nums[0];
+        int minVal = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
+
+                int temp = maxVal;
+                maxVal = minVal;
+                minVal = temp;
+            }
+
+            maxVal = Math.max(nums[i], maxVal * nums[i]);
+            minVal = Math.min(nums[i], minVal * nums[i]);
+
+            maxProduct = Math.max(maxProduct, maxVal);
+        }
+
+        return maxProduct;
+    }
+}
