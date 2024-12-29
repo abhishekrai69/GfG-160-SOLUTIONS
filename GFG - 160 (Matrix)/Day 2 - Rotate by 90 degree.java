7417f12a435edca1025429338f99a8658cfe899b
Abhishek Kumar Rai <122802561+abhishekrai69@users.noreply.github.com>
2024-12-29 19:54:05 +0530

    Create Day 2 - Rotate by 90 degree.java

---
+++ GFG - 160 (Matrix)/Day 2 - Rotate by 90 degree.java
@@ -0,0 +1,19 @@
+class Solution {
+    static void rotateby90(int mat[][]) {
+        int n = mat.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            for (int i = 0, k = n - 1; i < k; i++, k--) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[k][j];
+                mat[k][j] = temp;
+            }
+        }
+    }
+}
