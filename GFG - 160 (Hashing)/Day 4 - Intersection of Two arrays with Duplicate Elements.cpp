98bcf4e51addc0d343443746b05c47249f74bc9a
Abhishek Kumar Rai <122802561+abhishekrai69@users.noreply.github.com>
2024-12-29 19:49:43 +0530

    Create Day 4 - Intersection of Two arrays with Duplicate Elements.cpp

---
+++ GFG - 160 (Hashing)/Day 4 - Intersection of Two arrays with Duplicate Elements.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> intersectionWithDuplicates(vector<int>& a, vector<int>& b) {
+        unordered_set<int> sa(a.begin(), a.end());
+        unordered_set<int> res_set;
+        for (int num : b) {
+            if (sa.erase(num)) {  
+                res_set.insert(num);
+            }
+        }
+        return vector<int>(res_set.begin(), res_set.end());
+    }
+};
